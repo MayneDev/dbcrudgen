0f8b3271ad977b3bfb17da288d3f58294a7d7741
Victor Mwenda <vmwenda.vm@gmail.com>
2015-10-15 21:05:21 +0300

    PHPDatabaseUtilsTemplate
    PHPDatabaseUtilsTemplate

---
+++ java/src/com/marvik/apis/dbcrudgen/templates/php/PHPDatabaseUtilsTemplate.java
@@ -0,0 +1,49 @@
+package com.marvik.apis.dbcrudgen.templates.php;
+
+import java.io.IOException;
+
+import com.marvik.apis.dbcrudgen.filepaths.templates.TemplatesFilePath;
+import com.marvik.apis.dbcrudgen.templates.CrudTemplates;
+
+public class PHPDatabaseUtilsTemplate extends CrudTemplates {
+
+	/**
+	 * PHPDatabaseUtilsTemplate : The database utils template
+	 */
+	public PHPDatabaseUtilsTemplate() {
+
+	}
+
+	/**
+	 * Returns the PHP utils template
+	 */
+	@Override
+	public String openTemplate(String templateFilePath) throws IOException {
+		// TODO Auto-generated method stub
+		return super.openTemplate(templateFilePath);
+	}
+
+	/**
+	 * Returns the PHP utils template
+	 */
+	@Override
+	public String getTemplate() {
+		// TODO Auto-generated method stub
+		try {
+			return super.getTemplate();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	/**
+	 * Returns the PHP utils template file path
+	 */
+	@Override
+	public String getTemplateFilePath() {
+		// TODO Auto-generated method stub
+		return TemplatesFilePath.PHP_DATABASE_UTILS_TEMPLATE_FILE_PATH;
+	}
+}
