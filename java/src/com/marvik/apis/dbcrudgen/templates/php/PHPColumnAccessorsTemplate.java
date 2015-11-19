3403eee11d0ab40dca66b955a4c53739dcad09af
Victor Mwenda <vmwenda.vm@gmail.com>
2015-11-19 21:04:31 +0300

    PHPColumnAccessorsTemplate
    PHPColumnAccessorsTemplate

---
+++ java/src/com/marvik/apis/dbcrudgen/templates/php/PHPColumnAccessorsTemplate.java
@@ -0,0 +1,46 @@
+package com.marvik.apis.dbcrudgen.templates.php;
+
+import java.io.IOException;
+
+import com.marvik.apis.dbcrudgen.filepaths.templates.TemplatesFilePath;
+import com.marvik.apis.dbcrudgen.templates.CrudTemplates;
+
+public class PHPColumnAccessorsTemplate extends CrudTemplates{
+	
+	public PHPColumnAccessorsTemplate() {
+
+	}
+
+	/*
+	 * Returns the PHP columns access template
+	 */
+	@Override
+	public String openTemplate(String templateFilePath) throws IOException {
+		
+		return super.openTemplate(templateFilePath);
+	}
+
+	/*
+	 * Returns the PHP columns access template
+	 */
+	@Override
+	public String getTemplate(){
+		
+		try {
+			return super.getTemplate();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	/*
+	 * Returns the PHP columns access template file path
+	 */
+	@Override
+	public String getTemplateFilePath() {
+		
+		return TemplatesFilePath.PHP_COLUMNS_ACCESSORS_TEMPLATE_FILE_PATH;
+	}
+}
