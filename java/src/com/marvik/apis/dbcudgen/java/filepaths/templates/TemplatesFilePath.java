55634f5a6cd58290e14c5da9d538a13260d68f06
Victor Mwenda <vmwenda.vm@gmail.com>
2015-12-29 15:33:25 +0300

    Templates File Path
    Created a class to handle file paths

---
+++ java/src/com/marvik/apis/dbcudgen/java/filepaths/templates/TemplatesFilePath.java
@@ -0,0 +1,20 @@
+package com.marvik.apis.dbcudgen.java.filepaths.templates;
+
+public class TemplatesFilePath {
+
+	/*
+	 * COLUMNS_CRUD_TEMPLATE_FILE_PATH
+	 */
+	public static final String COLUMNS_CRUD_TEMPLATE_FILE_PATH = "res/templates/template_columns_crud.txt";
+
+	/*
+	 * ROUTINES_TEMPLATE_FILE_PATH
+	 */
+	public static final String ROUTINES_TEMPLATE_FILE_PATH = "res/templates/template_routines.txt";
+
+	/*
+	 * TABLES_TEMPLATE_FILE_PATH
+	 */
+	public static final String TABLES_TEMPLATE_FILE_PATH = "res/templates/template_tables.txt";
+
+}
