caf7688cbafd11cd701a51b7e7c23f590f309ede
Victor Mwenda <vmwenda.vm@gmail.com>
2015-12-29 15:14:44 +0300

    Tables Template
    Added a Tables Template

--- java/src/com/marvik/apis/dbcudgen/java/templates/TablesTemplate.java
+++ java/src/com/marvik/apis/dbcudgen/java/templates/TablesTemplate.java
@@ -1,5 +1,32 @@
 package com.marvik.apis.dbcudgen.java.templates;
 
-public class TablesTemplate {
+import java.io.IOException;
 
+import com.marvik.apis.dbcrudgen.java.io.FileStreamReader;
+
+public final class TablesTemplate {
+
+	private FileStreamReader fileStreamReader;
+	
+	
+	public TablesTemplate(){
+		fileStreamReader = new FileStreamReader();
+	}
+	/*
+	 * Returns the tables template
+	 */
+	private String openTablesTemplate(FileStreamReader fileStreamReader) throws IOException{
+		return fileStreamReader.readFile("res/templates/template_tables.txt");
+	}
+	/*
+	 * Returns the tables template
+	 */
+	public final String getTablesTemplate(){
+		try {
+			return openTablesTemplate(fileStreamReader);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
 }
