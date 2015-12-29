08c7f511f108586d0f305cbdc6acd885ed09812d
Victor Mwenda <vmwenda.vm@gmail.com>
2015-12-29 15:32:31 +0300

    Crud Template
    Added basic functionality to the Crud Template

--- java/src/com/marvik/apis/dbcudgen/java/templates/CrudTemplate.java
+++ java/src/com/marvik/apis/dbcudgen/java/templates/CrudTemplate.java
@@ -1,8 +1,38 @@
 package com.marvik.apis.dbcudgen.java.templates;
 
-public final class CrudTemplate {
+import java.io.IOException;
 
-	public CrudTemplate(){
-		
+import com.marvik.apis.dbcrudgen.java.io.FileStreamReader;
+
+public abstract class CrudTemplate {
+
+	private FileStreamReader fileStreamReader;
+
+	public CrudTemplate() {
+		fileStreamReader = new FileStreamReader();
 	}
+
+	/*
+	 * Returns the tables template
+	 */
+	private String openTemplate(String templateFilePath) throws IOException {
+		return fileStreamReader.readFile(templateFilePath);
+	}
+
+	/*
+	 * Returns the tables template
+	 */
+	public final String getTemplate(String templateFilePath) {
+		try {
+			return openTemplate(templateFilePath);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	/*
+	 * Returns the template file path
+	 */
+	public abstract String getTemplateFilePath();
 }
