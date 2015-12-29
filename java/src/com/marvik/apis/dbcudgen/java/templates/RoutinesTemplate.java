d4753f9cb3732b1cdc2a73aebbafe1cd81f1420b
Victor Mwenda <vmwenda.vm@gmail.com>
2015-12-29 15:13:59 +0300

    Routines Template
    Added Routines Template

--- java/src/com/marvik/apis/dbcudgen/java/templates/RoutinesTemplate.java
+++ java/src/com/marvik/apis/dbcudgen/java/templates/RoutinesTemplate.java
@@ -1,9 +1,26 @@
 package com.marvik.apis.dbcudgen.java.templates;
 
+import java.io.IOException;
+
+import com.marvik.apis.dbcrudgen.java.io.FileStreamReader;
+
 public class RoutinesTemplate {
 
 	public RoutinesTemplate(){
 		
+		
 	}
-	
+	/*
+	 * Returns the routines template
+	 */
+	private String openRoutinesTemplate(FileStreamReader fileStreamReader) throws IOException{
+		return fileStreamReader.readFile("res/templates/template_routines.txt");
+	}
+	/*
+	 * Returns the routines template
+	 */
+	public final String getRoutinesTemplate(){
+		return null;
+		
+	}
 }
