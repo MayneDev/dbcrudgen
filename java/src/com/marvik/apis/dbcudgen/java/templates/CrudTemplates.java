1549a838f520590e1c04a32156711e432e3156c0
Victor Mwenda <vmwenda.vm@gmail.com>
2015-12-29 16:05:01 +0300

    Created crud templates
    Created crud templates

---
+++ java/src/com/marvik/apis/dbcudgen/java/templates/CrudTemplates.java
@@ -0,0 +1,34 @@
+package com.marvik.apis.dbcudgen.java.templates;
+
+import java.io.IOException;
+
+import com.marvik.apis.dbcrudgen.java.io.FileStreamReader;
+
+public abstract class CrudTemplates {
+
+	private FileStreamReader fileStreamReader;
+
+	public CrudTemplates() {
+		fileStreamReader = new FileStreamReader();
+	}
+
+	/*
+	 * Returns the tables template
+	 */
+	public String openTemplate(String templateFilePath) throws IOException {
+		return fileStreamReader.readFile(templateFilePath);
+	}
+
+	/*
+	 * Returns the tables template
+	 */
+	public String getTemplate() throws IOException {
+		return openTemplate(getTemplateFilePath());
+
+	}
+
+	/*
+	 * Returns the template file path
+	 */
+	public abstract String getTemplateFilePath();
+}
