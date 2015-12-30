5e1047dbce1e569e38846abd13973160bd540ee1
Victor Mwenda <vmwenda.vm@gmail.com>
2015-12-30 20:21:50 +0300

    SQLRoutinesTemplate
    Created from old class RoutinesTemplate

---
+++ java/src/com/marvik/apis/dbcudgen/java/templates/sql/SQLRoutinesTemplate.java
@@ -0,0 +1,38 @@
+package com.marvik.apis.dbcudgen.java.templates.sql;
+
+import java.io.IOException;
+
+import com.marvik.apis.dbcudgen.java.filepaths.templates.TemplatesFilePath;
+import com.marvik.apis.dbcudgen.java.templates.CrudTemplates;
+
+public class SQLRoutinesTemplate extends CrudTemplates {
+
+	public SQLRoutinesTemplate() {
+
+	}
+
+	/*
+	 * Returns the routines template
+	 */
+
+	@Override
+	public String openTemplate(String templateFilePath) throws IOException {
+		// TODO Auto-generated method stub
+		return super.openTemplate(templateFilePath);
+	}
+
+	/*
+	 * Returns the routines template
+	 */
+	@Override
+	public String getTemplate() throws IOException {
+		// TODO Auto-generated method stub
+		return super.getTemplate();
+	}
+
+	@Override
+	public String getTemplateFilePath() {
+		// TODO Auto-generated method stub
+		return TemplatesFilePath.SQL_ROUTINES_TEMPLATE_FILE_PATH;
+	}
+}
