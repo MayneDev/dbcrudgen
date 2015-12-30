61a17d7da7f2131d1c80a21ea1e2d13026590deb
Victor Mwenda <vmwenda.vm@gmail.com>
2015-12-30 15:33:06 +0300

    DataTypes
    Added constructor,
    changed signature

--- java/src/com/marvik/apis/dbcrudgen/java/schemamodels/datatypes/DataType.java
+++ java/src/com/marvik/apis/dbcrudgen/java/schemamodels/datatypes/DataType.java
@@ -5,13 +5,42 @@ import com.marvik.apis.dbcrudgen.java.schemamodels.constraints.Constraints;
 public class DataType {
 
 	String dataType;
+	Constraints constraints;
 
-	public DataType(Constraints[] constraints) {
-		// TODO Auto-generated constructor stub
+	/**
+	 * @param dataType
+	 * @param constraints
+	 */
+	public DataType(String dataType, Constraints constraints) {
+		this.dataType = dataType;
+		this.constraints = constraints;
 	}
 
 	public String getDataType() {
 		return dataType;
+	}
+
+	/**
+	 * @return the constraints
+	 */
+	public Constraints getConstraints() {
+		return constraints;
+	}
+
+	/**
+	 * @param constraints
+	 *            the constraints to set
+	 */
+	public void setConstraints(Constraints constraints) {
+		this.constraints = constraints;
+	}
+
+	/**
+	 * @param dataType
+	 *            the dataType to set
+	 */
+	public void setDataType(String dataType) {
+		this.dataType = dataType;
 	}
 
 }
