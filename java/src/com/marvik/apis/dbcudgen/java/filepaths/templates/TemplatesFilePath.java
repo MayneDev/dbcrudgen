63a78be5d4ed1f2c300ba758f20534462ec5a554
Victor Mwenda <vmwenda.vm@gmail.com>
2015-12-30 20:25:02 +0300

    TemplatesFilePath
    Added new paths

--- java/src/com/marvik/apis/dbcudgen/java/filepaths/templates/TemplatesFilePath.java
+++ java/src/com/marvik/apis/dbcudgen/java/filepaths/templates/TemplatesFilePath.java
@@ -3,18 +3,27 @@ package com.marvik.apis.dbcudgen.java.filepaths.templates;
 public class TemplatesFilePath {
 
 	/*
-	 * COLUMNS_CRUD_TEMPLATE_FILE_PATH
+	 * PHP COLUMNS_ACCESSORS_TEMPLATE_FILE_PATH
 	 */
-	public static final String COLUMNS_CRUD_TEMPLATE_FILE_PATH = "res/templates/template_columns_crud.txt";
+	public static final String PHP_COLUMNS_ACCESSORS_TEMPLATE_FILE_PATH = "res/templates/php/template_php_table_column_accessors.txt";
+	/*
+	/*
+	 * PHP COLUMNS_CRUD_TEMPLATE_FILE_PATH
+	 */
+	public static final String PHP_TABLE_CLASS_CRUD_TEMPLATE_FILE_PATH = "res/templates/php/template_php_table_class_crud.txt";
+	/*
+	 * PHP COLUMNS_CRUD_TEMPLATE_FILE_PATH
+	 */
+	public static final String PHP_COLUMNS_CRUD_TEMPLATE_FILE_PATH = "res/templates/php/template_php_columns_crud.txt";
 
 	/*
-	 * ROUTINES_TEMPLATE_FILE_PATH
+	 * 	SQL ROUTINES_TEMPLATE_FILE_PATH
 	 */
-	public static final String ROUTINES_TEMPLATE_FILE_PATH = "res/templates/template_routines.txt";
+	public static final String SQL_ROUTINES_TEMPLATE_FILE_PATH = "res/templates/sql/template_sql_routines.txt";
 
 	/*
-	 * TABLES_TEMPLATE_FILE_PATH
+	 * SQL TABLES_TEMPLATE_FILE_PATH
 	 */
-	public static final String TABLES_TEMPLATE_FILE_PATH = "res/templates/template_tables.txt";
+	public static final String SQL_TABLES_TEMPLATE_FILE_PATH = "res/templates/sql/template_sql_tables.txt";
 
 }
