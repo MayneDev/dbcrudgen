ef8fe7acebc428b803822be294cc2486b306cffb
Victor Mwenda <vmwenda.vm@gmail.com>
2015-12-30 10:31:37 +0300

    Datatypes
    the various datatypes a database column can hold

---
+++ java/src/com/marvik/apis/dbcrudgen/java/schemamodels/datatypes/DataType.java
@@ -0,0 +1,17 @@
+package com.marvik.apis.dbcrudgen.java.schemamodels.datatypes;
+
+import com.marvik.apis.dbcrudgen.java.schemamodels.constraints.Constraints;
+
+public class DataType {
+
+	String dataType;
+
+	public DataType(Constraints[] constraints) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getDataType() {
+		return dataType;
+	}
+
+}
