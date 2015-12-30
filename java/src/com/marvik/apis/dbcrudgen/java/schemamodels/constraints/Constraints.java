aac119977b2b9f718de223922979eb9ad0124df4
Victor Mwenda <vmwenda.vm@gmail.com>
2015-12-30 10:29:29 +0300

    Constraints
    Class to handle all the constraints ina column

---
+++ java/src/com/marvik/apis/dbcrudgen/java/schemamodels/constraints/Constraints.java
@@ -0,0 +1,20 @@
+package com.marvik.apis.dbcrudgen.java.schemamodels.constraints;
+
+public class Constraints {
+	
+	private String constraint;
+	
+	public Constraints() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @return the constraint
+	 */
+	public String getConstraint() {
+		return constraint;
+	}
+	
+	
+
+}
