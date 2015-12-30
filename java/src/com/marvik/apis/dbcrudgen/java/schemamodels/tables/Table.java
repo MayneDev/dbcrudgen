7fa71d81a873dd46f81fa275a39657f723fedae4
Victor Mwenda <vmwenda.vm@gmail.com>
2015-12-30 12:23:30 +0300

    Tables
    Changed the signature of the constructor

--- java/src/com/marvik/apis/dbcrudgen/java/schemamodels/tables/Table.java
+++ java/src/com/marvik/apis/dbcrudgen/java/schemamodels/tables/Table.java
@@ -6,11 +6,13 @@ public class Table {
 
 	String tableName;
 	Columns[] columns;
+	String tableSql;
 
-	public Table(String tableName, Columns[] columns) {
+	public Table(String tableName, Columns[] columns, String tableSql) {
 		super();
 		this.tableName = tableName;
 		this.columns = columns;
+		this.tableSql = tableSql;
 	}
 
 	/**
@@ -25,6 +27,34 @@ public class Table {
 	 */
 	public Columns[] getColumns() {
 		return columns;
+	}
+
+	/**
+	 * @return the tableSql
+	 */
+	public String getTableSql() {
+		return tableSql;
+	}
+
+	/**
+	 * @param tableSql the tableSql to set
+	 */
+	public void setTableSql(String tableSql) {
+		this.tableSql = tableSql;
+	}
+
+	/**
+	 * @param tableName the tableName to set
+	 */
+	public void setTableName(String tableName) {
+		this.tableName = tableName;
+	}
+
+	/**
+	 * @param columns the columns to set
+	 */
+	public void setColumns(Columns[] columns) {
+		this.columns = columns;
 	}
 
 }
