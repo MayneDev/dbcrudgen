5ddeee896bf6ba6a5ba00d3891bfab821929d96f
Victor Mwenda <vmwenda.vm@gmail.com>
2015-12-30 12:24:13 +0300

    SQLReader
    Added code for reading sql files

--- java/src/com/marvik/apis/dbcrudgen/java/sql/reader/SQLReader.java
+++ java/src/com/marvik/apis/dbcrudgen/java/sql/reader/SQLReader.java
@@ -1,5 +1,29 @@
 package com.marvik.apis.dbcrudgen.java.sql.reader;
 
+import java.io.File;
+import java.io.IOException;
+
+import com.marvik.apis.dbcrudgen.java.io.FileStreamReader;
+
 public class SQLReader {
 
+	private FileStreamReader fileStreamReader;
+
+	public SQLReader() {
+		fileStreamReader = new FileStreamReader();
+	}
+
+	/*
+	 * Returns all the contents of an SQL file as a string
+	 */
+	public String getSQL(String filePath) throws IOException {
+		return fileStreamReader.readFile(filePath);
+	}
+
+	/*
+	 * Returns all the contents of an SQL file as a string
+	 */
+	public String getSQL(File file) throws IOException {
+		return getSQL(file.getAbsolutePath());
+	}
 }
