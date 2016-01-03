e575f68c8a33ddaccfdbe2ca8f33a6f02f03cabe
Victor Mwenda <vmwenda.vm@gmail.com>
2016-01-03 23:45:53 +0300

    TemplatesFilePath
    Changed block comments to javadoc style comments
    
    Added new template paths

--- java/src/com/marvik/apis/dbcrudgen/filepaths/templates/TemplatesFilePath.java
+++ java/src/com/marvik/apis/dbcrudgen/filepaths/templates/TemplatesFilePath.java
@@ -2,28 +2,46 @@ package com.marvik.apis.dbcrudgen.filepaths.templates;
 
 public class TemplatesFilePath {
 
-	/*
+	/**
 	 * PHP COLUMNS_ACCESSORS_TEMPLATE_FILE_PATH
 	 */
 	public static final String PHP_COLUMNS_ACCESSORS_TEMPLATE_FILE_PATH = "res/templates/php/template_php_table_column_accessors.txt";
-	/*
-	/*
+	
+	/**
 	 * PHP COLUMNS_CRUD_TEMPLATE_FILE_PATH
 	 */
 	public static final String PHP_TABLE_CLASS_CRUD_TEMPLATE_FILE_PATH = "res/templates/php/template_php_table_class_crud.txt";
-	/*
+	/**
 	 * PHP COLUMNS_CRUD_TEMPLATE_FILE_PATH
 	 */
 	public static final String PHP_COLUMNS_CRUD_TEMPLATE_FILE_PATH = "res/templates/php/template_php_columns_crud.txt";
 
-	/*
+	/**
+	 * PHP DATABASE_ACTIONS_TEMPLATE_FILE_PATH
+	 */
+	public static final String PHP_DATABASE_ACTIONS_TEMPLATE_FILE_PATH = "res/templates/php/template_php_database_actions.txt";
+	
+	/**
+	 * PHP DATABASE_CONNECTION_TEMPLATE_FILE_PATH
+	 */
+	public static final String PHP_DATABASE_CONNECTION_TEMPLATE_FILE_PATH = "res/templates/php/template_php_database_connect.txt";
+	
+	/**
+	 * PHP DATABASE_UTILS_TEMPLATE_FILE_PATH
+	 */
+	public static final String PHP_DATABASE_UTILS_TEMPLATE_FILE_PATH = "res/templates/php/template_php_database_utils.txt";
+	
+	/**
 	 * 	SQL ROUTINES_TEMPLATE_FILE_PATH
 	 */
 	public static final String SQL_ROUTINES_TEMPLATE_FILE_PATH = "res/templates/sql/template_sql_routines.txt";
 
-	/*
+	/**
 	 * SQL TABLES_TEMPLATE_FILE_PATH
 	 */
 	public static final String SQL_TABLES_TEMPLATE_FILE_PATH = "res/templates/sql/template_sql_tables.txt";
+	
+	
+	
 
 }
