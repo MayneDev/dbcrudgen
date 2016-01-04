001251f2df2539456c469bff24e7c7199908c24a
Victor Mwenda <vmwenda.vm@gmail.com>
2016-01-04 14:43:46 +0300

    NativeUtils
    Created NativeUtils class to hold native utils to be used by both the
    libraries and the standalone application

---
+++ java/src/com/marvik/apis/dbcrudgen/core/utils/NativeUtils.java
@@ -0,0 +1,17 @@
+package com.marvik.apis.dbcrudgen.core.utils;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public final class NativeUtils {
+
+	public static String getCurrentTime(String format, long timeInMillis) {
+		return getFormattedTime(format, timeInMillis);
+	}
+
+	public static String getFormattedTime(String format, long timeInMillis) {
+		return new SimpleDateFormat(format, Locale.getDefault()).format(new Date(timeInMillis));
+	}
+
+}
