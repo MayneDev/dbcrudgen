ab01b22bed1f44a8539a0b2840f1da8e7eefcdfc
Victor Mwenda <vmwenda.vm@gmail.com>
2016-01-05 10:55:55 +0300

    TemplateNativeFilePaths
    Created a file to hold native file paths that are reused by both the
    main application and the libraries

---
+++ java/src/com/marvik/apis/dbcrudgen/core/filepaths/templates/TemplateNativeFilePaths.java
@@ -0,0 +1,7 @@
+package com.marvik.apis.dbcrudgen.core.filepaths.templates;
+
+public class TemplateNativeFilePaths {
+
+	public static final String DB_CRUD_GEN_INFO_TEMPLATE_FILE_PATH = "res/dbcrudgen/native/template_php_db_crud_gen_info.txt";
+
+}
