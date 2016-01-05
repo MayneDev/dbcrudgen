c1ddacf0aec61afb4b91052eed8b1b5118791582
Victor Mwenda <vmwenda.vm@gmail.com>
2016-01-05 10:58:15 +0300

    DbCrudGeneratorNativeTemplates
    DbCrudGeneratorNativeTemplates used by both the standalone applications
    and the lllibraries

---
+++ java/src/com/marvik/apis/dbcrudgen/core/templates/DbCrudGeneratorNativeTemplates.java
@@ -0,0 +1,45 @@
+package com.marvik.apis.dbcrudgen.core.templates;
+
+import java.io.IOException;
+
+import com.marvik.apis.dbcrudgen.core.filepaths.templates.TemplateNativeFilePaths;
+import com.marvik.apis.dbcrudgen.templates.CrudTemplates;
+
+public class DbCrudGeneratorNativeTemplates extends CrudTemplates {
+
+	public DbCrudGeneratorNativeTemplates() {
+
+	}
+
+	/*
+	 * Returns the PHP class file name template
+	 */
+	@Override
+	public String openTemplate(String templateFilePath) throws IOException {
+
+		return super.openTemplate(templateFilePath);
+	}
+
+	/*
+	 * Returns the PHP class file name template
+	 */
+	@Override
+	public String getTemplate() {
+
+		try {
+			return super.getTemplate();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	/*
+	 * Returns the PHP class file name template file path
+	 */
+	@Override
+	public String getTemplateFilePath() {
+		return TemplateNativeFilePaths.DB_CRUD_GEN_INFO_TEMPLATE_FILE_PATH;
+	}
+}
