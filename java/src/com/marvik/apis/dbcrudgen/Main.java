70e2f205e596c805c23ef26253fd5cda7043261a
Victor Mwenda <vmwenda.vm@gmail.com>
2016-01-11 01:19:02 +0300

    Typos
    Fixed Typos

--- java/src/com/marvik/apis/dbcrudgen/Main.java
+++ java/src/com/marvik/apis/dbcrudgen/Main.java
@@ -41,18 +41,51 @@ public class Main {
 
 		List<Table> tablesList = new ArrayList<Table>();
 
+		addTableFirstAids(tablesList);
+		addTableFirstAidsCategories(tablesList);
+
+		Table[] tables = new Table[tablesList.size()];
+		for (int i = 0; i < tablesList.size(); i++) {
+			tables[i] = tablesList.get(i);
+		}
+
+		Database database = new Database("where_there_is_no_doc", tables);
+
+		PHPProjectConfiguration phpProjectConfiguration = new PHPProjectConfiguration("where_there_is_no_doc");
+		phpProjectConfiguration.setProjectStorageDirectory("C:\\xampp\\htdocs\\where_there_is_no_doc\\");
+		phpProjectConfiguration.setProjectCRUDScriptsStorageDirectory(
+				"C:\\xampp\\htdocs\\where_there_is_no_doc\\scripts\\php\\database\\crud\\");
+		phpProjectConfiguration.setProjectPHPDatabaseAPIScriptsStorageDirectory(
+				"C:\\xampp\\htdocs\\where_there_is_no_doc\\scripts\\php\\database\\core-apis\\");
+		phpProjectConfiguration.setProjectSQLScriptsStorageDirectory(
+				"C:\\xampp\\htdocs\\where_there_is_no_doc\\scripts\\php\\database\\sql\\");
+
+		ProjectDatabaseConnectionProperties projectDatabaseConnectionProperties = new ProjectDatabaseConnectionProperties(
+				"localhost", "root", "", "where_there_is_no_doc");
+
+		PHPCrudCreator phpCrudCreator = new PHPCrudCreator();
+		phpCrudCreator.setProjectConfiguration(phpProjectConfiguration);
+		phpCrudCreator.setProjectDatabaseConnectionProperties(projectDatabaseConnectionProperties);
+		phpCrudCreator.createProject(database);
+
+	}
+
+	/**
+	 * @param tablesList
+	 */
+	private static void addTableFirstAids(List<Table> tablesList) {
 		List<Columns> firstAidColumnList = new ArrayList<Columns>();
 		firstAidColumnList
 				.add(new Columns("ailment", new DataType("varchar", new Constraints("varchar(128) NOT NULL"))));
 		firstAidColumnList.add(new Columns("ailment_information", new DataType("text", new Constraints())));
 		firstAidColumnList.add(new Columns("ailment_causes", new DataType("text", new Constraints())));
-		firstAidColumnList.add(new Columns("ailement_prevention", new DataType("text", new Constraints())));
+		firstAidColumnList.add(new Columns("ailment_prevention", new DataType("text", new Constraints())));
 		firstAidColumnList.add(new Columns("ailment_signs", new DataType("text", new Constraints())));
 		firstAidColumnList.add(new Columns("ailment_symptoms", new DataType("text", new Constraints())));
 		firstAidColumnList.add(new Columns("ailment_cautions", new DataType("text", new Constraints())));
 		firstAidColumnList.add(new Columns("ailment_medication", new DataType("text", new Constraints())));
-		firstAidColumnList.add(new Columns("ailment_treatmeant", new DataType("text", new Constraints())));
-		firstAidColumnList.add(new Columns("ailment_treatmeant_precautions", new DataType("text", new Constraints())));
+		firstAidColumnList.add(new Columns("ailment_treatment", new DataType("text", new Constraints())));
+		firstAidColumnList.add(new Columns("ailment_treatment_precautions", new DataType("text", new Constraints())));
 		firstAidColumnList.add(new Columns("ailment_treatment_position", new DataType("text", new Constraints())));
 		firstAidColumnList.add(new Columns("ailment_short_notes", new DataType("text", new Constraints())));
 
@@ -71,32 +104,26 @@ public class Main {
 		PrimaryKeys primaryKeys = new PrimaryKeys(new String[] { "id_firstaid", });
 
 		tablesList.add(new Table("firstaids", firstAidColumns, tableSql, primaryKeys, null, null));
+	}
 
-		Table[] tables = new Table[tablesList.size()];
-		for (int i = 0; i < tablesList.size(); i++) {
-			tables[i] = tablesList.get(i);
+	/**
+	 * @param tablesList
+	 */
+	private static void addTableFirstAidsCategories(List<Table> tablesList) {
+		List<Columns> firstAidCategoriesColumnList = new ArrayList<Columns>();
+		firstAidCategoriesColumnList.add(new Columns("category_name", new DataType("varchar", new Constraints("varchar(128) NOT NULL"))));
+		
+		Columns[] firstAidColumns = new Columns[firstAidCategoriesColumnList.size()];
+		
+		for (int i = 0; i < firstAidCategoriesColumnList.size(); i++) {
+			firstAidColumns[i] = firstAidCategoriesColumnList.get(i);
 		}
+		String tableSql = "CREATE TABLE IF NOT EXISTS `firstaid_categories` ("
+				+ "`id_firstaid_category` int(11) NOT NULL," + " `category_name` varchar(128) NOT NULL)";
 
-		Database database = new Database("where_there_is_no_doc", tables);
-		
-		PHPProjectConfiguration phpProjectConfiguration = new PHPProjectConfiguration("where_there_is_no_doc");
-		phpProjectConfiguration.setProjectStorageDirectory("C:\\xampp\\htdocs\\where_there_is_no_doc\\");
-		phpProjectConfiguration.setProjectCRUDScriptsStorageDirectory("C:\\xampp\\htdocs\\where_there_is_no_doc\\scripts\\php\\database\\crud\\");
-		phpProjectConfiguration.setProjectPHPDatabaseAPIScriptsStorageDirectory("C:\\xampp\\htdocs\\where_there_is_no_doc\\scripts\\php\\database\\core-apis\\");
-		phpProjectConfiguration.setProjectSQLScriptsStorageDirectory("C:\\xampp\\htdocs\\where_there_is_no_doc\\scripts\\php\\database\\sql\\");
-		
-		ProjectDatabaseConnectionProperties projectDatabaseConnectionProperties = 
-				new ProjectDatabaseConnectionProperties("localhost","root","","where_there_is_no_doc");
-		
-		
-		
-		PHPCrudCreator phpCrudCreator = new PHPCrudCreator();
-		phpCrudCreator.setProjectConfiguration(phpProjectConfiguration);
-		phpCrudCreator.setProjectDatabaseConnectionProperties(projectDatabaseConnectionProperties);
-		phpCrudCreator.createProject(database);
-		
-		
-	
+		PrimaryKeys primaryKeys = new PrimaryKeys(new String[] { "id_firstaid_category", });
+
+		tablesList.add(new Table("firstaid_categories", firstAidColumns, tableSql, primaryKeys, null, null));
 	}
 
 	private static void print(String string) {
