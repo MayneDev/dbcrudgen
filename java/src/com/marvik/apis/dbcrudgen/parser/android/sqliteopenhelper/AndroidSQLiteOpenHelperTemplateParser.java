df7b8293bd6cfcbc18778eaf6d67263dd29649bc
Victor Mwenda <vmwenda.vm@gmail.com>
2016-01-16 22:54:54 +0300

    AndroidSQLiteOpenHelperTemplateParser
    Created AndroidSQLiteOpenHelperTemplateParser that creates custom
    SQLiteOpenHelper Subclass

---
+++ java/src/com/marvik/apis/dbcrudgen/parser/android/sqliteopenhelper/AndroidSQLiteOpenHelperTemplateParser.java
@@ -0,0 +1,74 @@
+package com.marvik.apis.dbcrudgen.parser.android.sqliteopenhelper;
+
+import com.marvik.apis.dbcrudgen.parser.TemplatesParser;
+import com.marvik.apis.dbcrudgen.parser.android.AndroidTemplatesParser;
+import com.marvik.apis.dbcrudgen.platforms.android.configuration.AndroidContentProviderConfiguration;
+import com.marvik.apis.dbcrudgen.platforms.android.configuration.AndroidDatabaseConfiguration;
+import com.marvik.apis.dbcrudgen.projects.android.configuration.AndroidProjectConfiguration;
+import com.marvik.apis.dbcrudgen.templates.simple.SimpleTemplates.FileNameTemplates;
+import com.marvik.apis.dbcrudgen.templates.tags.TemplateTags;
+
+public class AndroidSQLiteOpenHelperTemplateParser extends AndroidTemplatesParser {
+
+	public String createSQLiteOpenHelperSubclassSourceCode(AndroidProjectConfiguration androidProjectConfiguration) {
+
+		// Project storage directory
+		String projectStorageDir = androidProjectConfiguration.getProjectStorageDir();
+
+		// SQLite open helper class package
+		String sqliteOpenHelperSubclassPackage = androidProjectConfiguration.getAndroidContentProviderConfiguration()
+				.getAndroidDatabaseConfiguration().getSqliteOpenHelperClassPackage();
+
+		// SQLite open helper class
+		String sqliteOpenHelperSubclass = androidProjectConfiguration.getAndroidContentProviderConfiguration()
+				.getAndroidDatabaseConfiguration().getSqliteOpenHelperClass();
+
+		String sqliteOpenHelperSubclassTemplate = getAndroidClassSQLiteOpenHelperTemplate().getTemplate();
+
+		// add package name
+		sqliteOpenHelperSubclassTemplate = parseOpenHelperSubclassPackageName(sqliteOpenHelperSubclassTemplate,
+				sqliteOpenHelperSubclassPackage);
+
+		// add database tables class import
+		sqliteOpenHelperSubclassTemplate = addDatabaseTablesClassImport(sqliteOpenHelperSubclassTemplate,
+				androidProjectConfiguration);
+
+		// add sqlite open helper class name
+		sqliteOpenHelperSubclassTemplate = parseSQLiteOpenHelperSubClassName(sqliteOpenHelperSubclassTemplate,
+				sqliteOpenHelperSubclass);
+
+		return sqliteOpenHelperSubclassTemplate;
+	}
+
+	// add sqlite open helper class name
+	private String parseSQLiteOpenHelperSubClassName(String sqliteOpenHelperSubclassTemplate,
+			String sqliteOpenHelperSubclass) {
+		return sqliteOpenHelperSubclassTemplate.replace(TemplateTags.Android.SQLITE_OPEN_HELPER_SUBCLASS,
+				sqliteOpenHelperSubclass);
+	}
+
+	// add database tables class import
+	private String addDatabaseTablesClassImport(String sqliteOpenHelperSubclassTemplate,
+			AndroidProjectConfiguration androidProjectConfiguration) {
+
+		AndroidContentProviderConfiguration androidContentProviderConfiguration = androidProjectConfiguration
+				.getAndroidContentProviderConfiguration();
+
+		AndroidDatabaseConfiguration androidDatabaseConfiguration = androidContentProviderConfiguration
+				.getAndroidDatabaseConfiguration();
+
+		String tableSchemasPackage = androidDatabaseConfiguration.getTablesSchemasPackage();
+		tableSchemasPackage = parsePackageName(tableSchemasPackage);
+
+		String tableSchemasClass = tableSchemasPackage + TemplateTags.TAG_PRINTING_CHAR_DOT
+				+ FileNameTemplates.Android.TABLE_SCHEMAS_CLASS_NAME;
+		return sqliteOpenHelperSubclassTemplate.replace(TemplateTags.Android.DATABASE_TABLES_CLASS, tableSchemasClass);
+	}
+
+	private String parseOpenHelperSubclassPackageName(String sqliteOpenHelperSubclassTemplate,
+			String sqliteOpenHelperSubclassPackage) {
+		String packageName = parsePackageName(sqliteOpenHelperSubclassPackage);
+		return sqliteOpenHelperSubclassTemplate.replace(TemplateTags.Android.PACKAGE_NAME, packageName);
+	}
+
+}
