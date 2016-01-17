25a305fa610d539ee360910d2511543aaadc4f1e
Victor Mwenda <vmwenda.vm@gmail.com>
2016-01-17 18:45:49 +0300

    Native Utils
    Added new methods

--- java/src/com/marvik/apis/dbcrudgen/core/utils/NativeUtils.java
+++ java/src/com/marvik/apis/dbcrudgen/core/utils/NativeUtils.java
@@ -5,6 +5,10 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 
+import com.marvik.apis.dbcrudgen.core.platforms.java.grammar.JavaGrammar;
+import com.marvik.apis.dbcrudgen.core.platforms.java.grammar.delimeters.JavaDelimiter;
+import com.marvik.apis.dbcrudgen.core.templates.tags.NativeTemplateTags;
+import com.marvik.apis.dbcrudgen.templates.simple.SimpleTemplates;
 import com.marvik.apis.dbcrudgen.templates.tags.TemplateTags;
 
 public final class NativeUtils {
@@ -35,20 +39,74 @@ public final class NativeUtils {
 
 	public static CharSequence getClassVariable(String filename) {
 		filename = filename.replace(".java", "");
-		return filename.substring(0, 1).toLowerCase()+filename.substring(1, filename.length());
+		return filename.substring(0, 1).toLowerCase() + filename.substring(1, filename.length());
 	}
-	
+
 	public static String getString(int integer) {
 		return String.format("%d", integer);
 	}
 
 	/**
-	 * NativeUtils#parsePackageName
-	 * Formats a file path into a java package url
+	 * NativeUtils#parsePackageName Formats a file path into a java package url
+	 * 
 	 * @param packageFilePath
 	 * @return
 	 */
 	public static String parseJavaPackage(String packageFilePath) {
 		return packageFilePath.replace(NativeUtils.getFileSeparator(), TemplateTags.TAG_PRINTING_CHAR_DOT);
+	}
+
+	public static String createJavaVariable(String dataType, String objectName, JavaDelimiter javaDelimeter) {
+		String javaVariable = "";
+
+		if (dataType.equalsIgnoreCase("INT")) {
+			dataType = "int";
+		}
+		if (dataType.equalsIgnoreCase("INTEGER")) {
+			dataType = "int";
+		}
+		if (dataType.equalsIgnoreCase("STRING")) {
+			dataType = "String";
+		}
+		if (dataType.equalsIgnoreCase("TEXT")) {
+			dataType = "String";
+		}
+		if (dataType.equalsIgnoreCase("VARCHAR")) {
+			dataType = "String";
+		}
+		if (dataType.equalsIgnoreCase("FLOAT")) {
+			dataType = "float";
+		}
+		if (dataType.equalsIgnoreCase("DOUBLE")) {
+			dataType = "double";
+		}
+		if (dataType.equalsIgnoreCase("BYTE")) {
+			dataType = "byte";
+		}
+
+		javaVariable = dataType + JavaGrammar.SPACE + objectName;
+
+		switch (javaDelimeter) {
+		case COMMA:
+			javaVariable += ",";
+			break;
+		case SEMICOLON:
+			javaVariable += ";";
+			break;
+		case DOT:
+			javaVariable += ".";
+			break;
+		case NONE:
+
+			javaVariable += "";
+			break;
+		}
+
+		return javaVariable;
+	}
+
+	public static CharSequence parseStringDefaultParser(String variable) {
+		String stringParserTemplate = SimpleTemplates.Java.STRING_DEFAULT_PARSER;
+		return stringParserTemplate.replace(NativeTemplateTags.Java.JAVA_OBJECT_TAG, variable);
 	}
 }
