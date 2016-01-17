6089dd46e13a00ffed1146a004ba03547e920e0d
Victor Mwenda <vmwenda.vm@gmail.com>
2016-01-17 18:45:19 +0300

    NativeTemplateTags
    Added new class and native template tag

--- java/src/com/marvik/apis/dbcrudgen/core/templates/tags/NativeTemplateTags.java
+++ java/src/com/marvik/apis/dbcrudgen/core/templates/tags/NativeTemplateTags.java
@@ -2,6 +2,16 @@ package com.marvik.apis.dbcrudgen.core.templates.tags;
 
 public class NativeTemplateTags {
 
+	/**
+	 * SYSTEM_WRITE_TIME
+	 */
 	public static final String SYSTEM_WRITE_TIME = "$SYSTEM_WRITE_TIME";
+
+	public static class Java {
+		/**
+		 * $OBJECT
+		 */
+		public static final String JAVA_OBJECT_TAG = "$OBJECT";
+	}
 
 }
