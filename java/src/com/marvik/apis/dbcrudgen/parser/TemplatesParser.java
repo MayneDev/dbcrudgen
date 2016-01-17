d096fa2aed4a6aa671b9468743db36df0a529124
Victor Mwenda <vmwenda.vm@gmail.com>
2016-01-17 03:02:47 +0300

    TemplatesParser
    Added javadoc comments

--- java/src/com/marvik/apis/dbcrudgen/parser/TemplatesParser.java
+++ java/src/com/marvik/apis/dbcrudgen/parser/TemplatesParser.java
@@ -1,8 +1,14 @@
 package com.marvik.apis.dbcrudgen.parser;
 
+/**
+ * Templates Parser - Used for parsing templates into meaningful source files
+ * 
+ * @author victor
+ *
+ */
 public class TemplatesParser {
 
 	public TemplatesParser() {
-		
+
 	}
 }
