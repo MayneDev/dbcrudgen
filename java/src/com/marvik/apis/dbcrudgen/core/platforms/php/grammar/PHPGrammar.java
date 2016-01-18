53ce30697fc5d52b9d4f75fe20c8c20b7fbd8489
Victor Mwenda <vmwenda.vm@gmail.com>
2016-01-18 21:44:43 +0300

    PHPGrammar
    Created a php grammar class

---
+++ java/src/com/marvik/apis/dbcrudgen/core/platforms/php/grammar/PHPGrammar.java
@@ -0,0 +1,8 @@
+package com.marvik.apis.dbcrudgen.core.platforms.php.grammar;
+
+public class PHPGrammar {
+
+	public static final class Variables {
+		public static final String PHP_VARIABLE_PREFIX = "$";
+	}
+}
