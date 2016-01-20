f834062d24e07a29425fadd490507dbdeec7302d
Victor Mwenda <vmwenda.vm@gmail.com>
2016-01-20 11:28:34 +0300

    Android project configuration
    Added 4 more varibles : javaSrcDirs source set, and java package
    name.....added two more varibles which are dyna,mically computed
    
    private String _javaFilesStorageLocation;
    private String _projectDefaultJavaFilesRootStorageLocation;

--- java/src/com/marvik/apis/dbcrudgen/projects/android/configuration/AndroidProjectConfiguration.java
+++ java/src/com/marvik/apis/dbcrudgen/projects/android/configuration/AndroidProjectConfiguration.java
@@ -1,16 +1,28 @@
 package com.marvik.apis.dbcrudgen.projects.android.configuration;
 
+import com.marvik.apis.dbcrudgen.core.utils.NativeUtils;
 import com.marvik.apis.dbcrudgen.platforms.android.configuration.AndroidContentProviderConfiguration;
 
 public class AndroidProjectConfiguration {
 
+	private String _javaFilesStorageLocation;
+	private String _projectDefaultJavaFilesRootStorageLocation;
+
 	private String projectStorageDir;
+	private String javaSrcDir;
+	private String packageName;
 	private AndroidContentProviderConfiguration androidContentProviderConfiguration;
 
-	public AndroidProjectConfiguration(String projectStorageDir,
+	public AndroidProjectConfiguration(String projectStorageDir, String javaSrcDir, String packageName,
 			AndroidContentProviderConfiguration androidContentProviderConfiguration) {
 		this.androidContentProviderConfiguration = androidContentProviderConfiguration;
 		this.projectStorageDir = projectStorageDir;
+		this.packageName = packageName;
+		this.javaSrcDir = javaSrcDir;
+
+		// ActualPath Where Java Files are saved
+		_setJavaFilesStorageLocation();
+
 	}
 
 	/**
@@ -21,7 +33,8 @@ public class AndroidProjectConfiguration {
 	}
 
 	/**
-	 * @param projectStorageDir the projectStorageDir to set
+	 * @param projectStorageDir
+	 *            the projectStorageDir to set
 	 */
 	public void setProjectStorageDir(String projectStorageDir) {
 		this.projectStorageDir = projectStorageDir;
@@ -35,12 +48,78 @@ public class AndroidProjectConfiguration {
 	}
 
 	/**
-	 * @param androidContentProviderConfiguration the androidContentProviderConfiguration to set
+	 * @param androidContentProviderConfiguration
+	 *            the androidContentProviderConfiguration to set
 	 */
 	public void setAndroidContentProviderConfiguration(
 			AndroidContentProviderConfiguration androidContentProviderConfiguration) {
 		this.androidContentProviderConfiguration = androidContentProviderConfiguration;
 	}
-	
-	
+
+	/**
+	 * @return the javaSrcDir
+	 */
+	public String getJavaSrcDir() {
+		return javaSrcDir;
+	}
+
+	/**
+	 * @param javaSrcDir
+	 *            the javaSrcDir to set
+	 */
+	public void setJavaSrcDir(String javaSrcDir) {
+		this.javaSrcDir = javaSrcDir;
+	}
+
+	/**
+	 * @return the packageName
+	 */
+	public String getPackageName() {
+		return packageName;
+	}
+
+	/**
+	 * @param packageName
+	 *            the packageName to set
+	 */
+	public void setPackageName(String packageName) {
+		this.packageName = packageName;
+	}
+
+	/**
+	 * Sets the path to the java files storage location. This is usually the
+	 * path to the src folder
+	 */
+	private void _setJavaFilesStorageLocation() {
+		_javaFilesStorageLocation = getProjectStorageDir() + NativeUtils.getFileSeparator() + getJavaSrcDir();
+		_setProjectDefaultJavaFilesRootStorageLocation();
+	}
+
+	/**
+	 * The path of the java files storage location
+	 * 
+	 * @return _javaFilesStorageLocation
+	 */
+	public String _getJavaFilesStorageLocation() {
+		return _javaFilesStorageLocation;
+	}
+
+	/**
+	 * Sets the default path where java files are in the project This is usually
+	 * the project name storage path + java src folder + the project package
+	 * folder(which is generated from the package name
+	 */
+	private void _setProjectDefaultJavaFilesRootStorageLocation() {
+		String packageNameAsPath = NativeUtils.parsePackagePath(getPackageName());
+		_projectDefaultJavaFilesRootStorageLocation = _javaFilesStorageLocation + NativeUtils.getFileSeparator() + packageNameAsPath;
+	}
+	/**
+	 * Returns the default path where java files are in the project This is usually
+	 * the project name storage path + java src folder + the project package
+	 * folder(which is generated from the package name
+	 */
+	public String _getProjectDefaultJavaFilesRootStorageLocation() {
+		return _projectDefaultJavaFilesRootStorageLocation;
+	}
+
 }
