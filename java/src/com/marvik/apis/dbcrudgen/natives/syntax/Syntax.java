f865801e1fcd84b5ef3d87a909cfbf907f193d0b
Victor Mwenda <vmwenda.vm@gmail.com>
2016-01-28 14:49:07 +0300

    Syntax
    Created a class to hold syntaxes

---
+++ java/src/com/marvik/apis/dbcrudgen/natives/syntax/Syntax.java
@@ -0,0 +1,47 @@
+package com.marvik.apis.dbcrudgen.natives.syntax;
+
+/**
+ * Syntax
+ * 
+ * The syntax of various items, languages etc.
+ * 
+ * @author victor
+ *
+ */
+public class Syntax {
+
+	/**
+	 * PrintingChars
+	 * 
+	 * Various Printing characters and symbols
+	 * 
+	 * @author victor
+	 *
+	 */
+	public class PrintingChars {
+
+		/**
+		 * ESCAPED_DOUBLE_QUOTES
+		 */
+		public static final String ESCAPED_DOUBLE_QUOTES = "\"";
+
+	}
+
+	/**
+	 * ArithmeticChars
+	 * 
+	 * Various Arithmetic characters and symbols
+	 * 
+	 * @author victor
+	 *
+	 */
+	public class ArithmeticChars {
+
+		/**
+		 * ADD
+		 */
+		public static final String ADD = "+";
+
+	}
+
+}
