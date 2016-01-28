f884da6c1bd936b7cf711e69cab2400f7b38c388
Victor Mwenda <vmwenda.vm@gmail.com>
2016-01-28 14:48:00 +0300

    SQLGrammar
    Created class SQL Grammar

---
+++ java/src/com/marvik/apis/dbcrudgen/core/platforms/sql/grammar/SQLGrammar.java
@@ -0,0 +1,34 @@
+package com.marvik.apis.dbcrudgen.core.platforms.sql.grammar;
+
+/**
+ * SQLGrammar
+ * 
+ * Structured Query Language Grammar - Contains the used Grammar of the SQL
+ * programming language used in developing any part of this application/library
+ * 
+ * @author victor
+ *
+ */
+public class SQLGrammar {
+
+	/**
+	 * Keywords
+	 * 
+	 * Contains any of the SQL keywords used in this application
+	 * 
+	 * @author victor
+	 *
+	 */
+	public static class Keywords {
+
+		/**
+		 * AND
+		 */
+		public static final String AND = " AND ";
+
+		/**
+		 * OR
+		 */
+		public static final String OR = " OR ";
+	}
+}
