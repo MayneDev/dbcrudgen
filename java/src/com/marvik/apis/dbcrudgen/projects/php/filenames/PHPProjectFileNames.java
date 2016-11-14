de55d16e0b116de2bf026fe796ea37e40cd3dda4
Victor Mwenda <vmwenda.vm@gmail.com>
2016-11-14 21:02:28 +0300

    PHPProjectFileNames
    PHPProjectFileNames

---
+++ java/src/com/marvik/apis/dbcrudgen/projects/php/filenames/PHPProjectFileNames.java
@@ -0,0 +1,10 @@
+package com.marvik.apis.dbcrudgen.projects.php.filenames;
+
+public class PHPProjectFileNames {
+
+	public static final String DATABASE_UTILS_SCRIPT_FILENAME = "DatabaseUtils.class.inc.php";
+
+	public static final String DATABASE_CONNECTION_SCRIPT_FILENAME = "DatabaseConnection.class.inc.php";
+
+	public static final String DATA_ACTIONS_SCRIPT_FILENAME = "DatabaseActions.class.inc.php";
+}
